d51660ff37f91618ba4f5586c03ff1e63ecafd65
Marfat Abdullahi <marfat@lightful.com>
2017-11-22 09:59:36 +0000

    Started learning react native, what exciting times

---
+++ README.md
@@ -0,0 +1,12 @@
+# React Native
+
+## Requirements
+0. Node >= 7.72
+
+## Setup
+0. `cd app`
+0. `yarn start`
+
+For Android emulator press  `a`
+
+For iOS emulator press `i`
---
+++ app/App.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import { StyleSheet, Text, TextInput, View, Image, Button } from "react-native";
+import { setInterval } from "core-js/library/web/timers";
+
+class Header extends Component {
+  render() {
+    return (
+      <Text>Hello, {this.props.name}!</Text>
+    );
+  }
+}
+
+class Monkeys extends Component {
+  render() {
+    const pic = {
+      uri: "http://www.monkeyworld.org/images/monkeys.jpg"
+    };
+    return (
+      <Image source={pic} style={{ width: 193, height: 110 }} />
+    );
+  }
+}
+
+class Mood extends Component {
+  constructor(props) {
+    super(props);
+    const moods = ["happy", "sad"]
+    this.state = { mood: moods[0] };
+    console.log('thy props', props)
+
+    setInterval(() => {
+      if (this.props.rotate) {
+        this.setState(previousState => {
+          return { mood: (previousState.mood == moods[0]) ? moods[1] : moods[0] }
+        })
+      }
+    }, 3000);
+  }
+  render() {
+    return (
+      <Text>My current is {this.state.mood}</Text>
+    )
+  }
+}
+
+class Initiate extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { answer: '' };
+  }
+  render() {
+    return (
+      <View>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="I'm ready..."
+          onChangeText={(answer) => this.setState({ answer })}
+        />
+        <Text>{this.state.answer}</Text>
+      </View>
+    )
+  }
+}
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isChanging: true };
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Header name="Monkey Business" />
+        <Monkeys />
+        <Mood rotate={this.state.isChanging} />
+
+        <Text>You ready for the real magic?</Text>
+        <Initiate />
+
+        <Button
+          onPress={() => { console.log('change state now') }}
+          title="Press Me"
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
